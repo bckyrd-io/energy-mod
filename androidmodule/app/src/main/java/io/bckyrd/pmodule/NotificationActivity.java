ab3795cbb895f4fcb9ad01279eddf3d35c05d00b
h0zt <host@bckyrd.io>
2023-03-13 03:21:45 -0700

    optimize statistics and distribute
    fixed the androidmpchart to show only 3 last data entries
    and also the distribute to change node
    button to reflect action on or off

--- androidmodule/app/src/main/java/io/bckyrd/pmodule/NotificationActivity.java
+++ androidmodule/app/src/main/java/io/bckyrd/pmodule/NotificationActivity.java
@@ -1,12 +1,34 @@
 package io.bckyrd.pmodule;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationActivity extends AppCompatActivity {
+    private ListView noteListView;
+    private NoteAdapter noteAdapter;
+
+    private DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +38,134 @@ public class NotificationActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("Notification");
+
+        noteListView = findViewById(R.id.note_list_view);
+        noteAdapter = new NoteAdapter(this, new ArrayList<Note>());
+
+        noteListView.setAdapter(noteAdapter);
+
+
+
+        // Get a reference to the Firebase database
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        // Add dummy data to the database
+        /**
+        Note note1 = new Note("Note 1", "2022-03-12 10:00:00");
+        String note1Id = databaseReference.child("notes").push().getKey();
+        databaseReference.child("notes").child(note1Id).setValue(note1);
+
+        Note note2 = new Note("Note 2", "2022-03-12 11:00:00");
+        String note2Id = databaseReference.child("notes").push().getKey();
+        databaseReference.child("notes").child(note2Id).setValue(note2);
+         */
+
+        // Read the notes from the database and add them to the list
+
+
+
+
+
+        // Read the notes from the database and add them to the list
+        databaseReference.child("notes").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Note> noteList = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Note note = snapshot.getValue(Note.class);
+                    noteList.add(note);
+                }
+                noteAdapter.clear();
+                noteAdapter.addAll(noteList);
+                noteAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("NotificationActivity", "loadNotes:onCancelled", databaseError.toException());
+            }
+        });
     }
+}
+
+class NoteAdapter extends BaseAdapter {
+    private List<Note> notes;
+    private Context context;
+
+    public NoteAdapter(Context context, List<Note> notes) {
+        this.context = context;
+        this.notes = notes;
+    }
+
+    @Override
+    public int getCount() {
+        return notes.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return notes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.note_row, parent, false);
+        }
+
+        Note note = notes.get(position);
+
+        TextView noteTxt = convertView.findViewById(R.id.note_txt);
+        TextView noteTime = convertView.findViewById(R.id.note_time);
+
+        // Set the text and time for the note
+        noteTxt.setText(note.getText());
+        noteTime.setText(note.getTime());
+        return convertView;
+    }
+
+    public void clear() {
+        notes.clear();
+    }
+
+    public void addAll(List<Note> notes) {
+        this.notes.addAll(notes);
+    }
+
 }
 
 
 
+class Note {
+    private String text;
+    private String time;
 
+    public Note() {
+    }
 
+    public Note(String text, String time) {
+        this.text = text;
+        this.time = time;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
--- androidmodule/app/src/main/java/io/bckyrd/pmodule/StatisticsActivity.java
+++ androidmodule/app/src/main/java/io/bckyrd/pmodule/StatisticsActivity.java
@@ -1,11 +1,33 @@
 package io.bckyrd.pmodule;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.ActionBar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StatisticsActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +37,102 @@ public class StatisticsActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("Statistics");
+
+        // Find the LineChart view by its ID
+        //LineChart lineChart = findViewById(R.id.lineChart);
+
+        // Get a reference to the "voltage" node in the Firebase database
+       // DatabaseReference voltageRef = FirebaseDatabase.getInstance().getReference().child("voltage");
+
+        //Insert data entries into the "voltage" node
+        /*
+        voltageRef.push().setValue(new Entry(2, 230));
+        voltageRef.push().setValue(new Entry(3, 240));
+        voltageRef.push().setValue(new Entry(4, 220));
+         */
+
+        // Create a LineChart object
+        LineChart lineChart = findViewById(R.id.lineChart);
+
+// Get a reference to the Firebase Realtime Database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference voltageRef = database.getReference("voltage");
+
+// Add a ValueEventListener to the voltageRef reference
+        voltageRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Entry> dataEntries = new ArrayList<>();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    float x = snapshot.child("x").getValue(Float.class);
+                    float y = snapshot.child("y").getValue(Float.class);
+                    dataEntries.add(new Entry(x, y));
+                }
+
+                // Show only the last 3 data entries
+                int startIndex = dataEntries.size() > 3 ? dataEntries.size() - 3 : 0;
+                List<Entry> lastThreeEntries = dataEntries.subList(startIndex, dataEntries.size());
+
+                // Create a LineDataSet object and customize its appearance
+                LineDataSet dataSet = new LineDataSet(lastThreeEntries, "");
+                dataSet.setColor(Color.LTGRAY);
+                dataSet.setDrawCircles(true);
+                dataSet.setCircleColor(Color.BLACK);
+                dataSet.setCircleRadius(5f);
+                dataSet.setDrawValues(true);
+                dataSet.setValueTextColor(Color.BLACK);
+                dataSet.setValueTextSize(12f);
+                dataSet.setValueFormatter(new ValueFormatter() {
+                    @Override
+                    public String getAxisLabel(float value, AxisBase axis) {
+                        return String.valueOf((int) value);
+                    }
+                });
+
+                // Create a LineData object and add the LineDataSet to it
+                LineData data = new LineData(dataSet);
+
+                // Set some custom formatting for the X axis
+                XAxis xAxis = lineChart.getXAxis();
+                xAxis.setGranularity(1f);
+                xAxis.setGranularityEnabled(true);
+                xAxis.setDrawGridLines(false);
+                xAxis.setDrawAxisLine(false);
+                xAxis.setDrawLabels(false);
+
+                // Set some custom formatting for the Y axis
+                YAxis yAxis = lineChart.getAxisLeft();
+                yAxis.setDrawGridLines(false);
+                yAxis.setDrawAxisLine(false);
+                yAxis.setGranularityEnabled(true);
+                yAxis.setDrawLabels(false);
+
+                // Hide the right Y axis
+                lineChart.getAxisRight().setEnabled(false);
+
+                // Disable interactions with the chart
+                lineChart.setTouchEnabled(false);
+                lineChart.setDragEnabled(false);
+                lineChart.setScaleEnabled(false);
+                lineChart.setPinchZoom(false);
+
+                // Remove the description label
+                lineChart.getDescription().setEnabled(false);
+
+                // Set the LineData object as the data for the LineChart view
+                lineChart.setData(data);
+
+                // Invalidate the LineChart view to refresh it
+                lineChart.invalidate();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 }
-
-
-
-
-
